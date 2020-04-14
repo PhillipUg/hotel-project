e36058be99dba62e1ed0a2557ca89d1a5e2a82ac
phillip musiime <phillipmusiime@gmail.com>
2020-04-14 16:37:42 +0300

    added room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,20 @@
 class Room
+  attr_accessor :capacity, :occupants
 
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    @occupants.size < @capacity ? false : true
+  end
+
+  def available_space
+    @capacity - @occupants.size
+  end
+
+  def add_occupant(name)
+    !self.full? ? (@occupants << name; true) : false
+  end
 end
